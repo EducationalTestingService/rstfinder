fd729227c9db264f9727ffc9f39ba84d8553ad15
Daniel Blanchard <dblanchard@ets.org>
2014-06-10 13:51:16 -0400

    First attempt at adding 'training' mode.

--- discourseparsing/discourse_parsing.py
+++ discourseparsing/discourse_parsing.py
@@ -34,7 +34,9 @@ originally written by Kenji Sagae in perl.
 '''
 
 import re
+import sys
 from collections import defaultdict, namedtuple
+from operator import attrgetter, itemgetter
 
 import numpy as np
 
@@ -205,8 +207,7 @@ class Parser(object):
         # Don't allow a reduce right or left if there are not
         # at least two items in the stack to be reduced
         # (plus the leftwall).
-        if re.search(r'^[RL]', act) \
-                and act != "R:ROOT" and len(stack) < 3:
+        if re.search(r'^[RL]', act) and act != "R:ROOT" and len(stack) < 3:
             return False
 
         # Default: the action is valid.
@@ -359,7 +360,7 @@ class Parser(object):
             res.append(tmp_item)
         return res
 
-    def parse(self, edus, train_mode=False):
+    def parse(self, edus, train_file=None):
         '''
         edus is a list of (word, pos) tuples
         '''
@@ -367,20 +368,17 @@ class Parser(object):
         gold_acts = []
         states = []
         completetrees = []
+        num_parse_errors = 0
 
         sent = self.initialize_edu_data(edus)
 
         # if we are training, the gold actions should be
-        # in the input file
-        if train_mode:
-            # TODO
-            # <>;    #blank line
-            #     my $actstr = <>;
-            #     $actstr =~ s/[\n\r]//g;
-            # put the gold actions in @goldacts
-            #     @goldacts = split /[ \t]+/, $actstr;
-            # <>;    #blank line
-            pass
+        # in the training file
+        if train_file is not None:
+            for line in train_file:
+                line = line.strip()
+                if line.startswith('S:'):
+                    gold_acts.extend(line.split())
 
         # initialize the stack
         stack = []
@@ -418,7 +416,7 @@ class Parser(object):
 
         # loop while there are states to process
         while states:
-            states.sort(key=lambda x: x['score'], reverse=True)
+            states.sort(key=itemgetter('score'), reverse=True)
             if len(states) > self.max_states:
                 states = states[:self.max_states]
 
@@ -428,7 +426,7 @@ class Parser(object):
                 # check if the current state corresponds to a complete tree
                 completetrees.append({'tree': cur_state["stack"][0]["tree"],
                                       'score': cur_state["score"]})
-                if train_mode or len(completetrees) >= self.n_best:
+                if train_file is not None or len(completetrees) >= self.n_best:
                     break
 
             stack = cur_state["stack"]
@@ -442,40 +440,33 @@ class Parser(object):
             # Compute the possible actions given this state.
             # During training, print them out.
             # During parsing, score them according to the model and sort.
-            if train_mode:
-                scored_acts = []
-                pass
-                # TODO
-                # take the next action from @goldacts
-                # my $tmpstr = shift @goldacts;
-                # $scored_acts[0] = {
-                #     act   => $tmpstr,
-                #     score => 1,
-                # };
-                # if ( $scored_acts[0]->{act} eq "" ) {
-                #     $numparseerror++;
-                #     print STDERR
-                #         "Parse error (no more actions). $numparseerror\n";
-                #     last;
-                # }
+            scored_acts = []
+            if train_file is not None:
+                # take the next action from gold_acts
+                action_str = gold_acts.pop(0) if gold_acts else ''
+                if not action_str:
+                    # import ipdb; ipdb.set_trace()
+                    print('Parse error (no more actions) %s' %
+                          (num_parse_errors), file=sys.stderr)
+                    num_parse_errors += 1
+                    break
 
-                # $scored_acts[0]->{act} =~ s/^S:(.*)$/S:POS/;
-                # $scored_acts[0]->{act} =~ s/^([^\=\-]+)[\=\-].+/$1/;
+                action_str = re.sub(r'^S:(.*)$', r'S:POS',
+                                            action_str)
+                action_str = re.sub(r'^([^\=\-]+)[\=\-].+', r'$1',
+                                            action_str)
 
-                # if (!(     ( $scored_acts[0]->{act} eq $prevact )
-                #         && ( $scored_acts[0]->{act} =~ /^U/ )
-                #     )
-                #     )
-                # {
-                #     my $featstr = join " ", @{$featsref};
-                #     print "$scored_acts[0]->{act} $featstr\n";
-                # }
+                if not (action_str == cur_state["prevact"] and
+                        action_str.startswith('U')):
+                    print(' '.join([action_str] + feats))
+
+                scored_acts.append(ScoredAction(action_str, 1))
             else:
                 scored_acts = self.mxclassify(feats)
                 scored_acts = sorted(scored_acts,
-                                     key=lambda x: x.score,
+                                     key=attrgetter('score'),
                                      reverse=True)
-                #import sys; print('\n'.join(['{} {:.4g}'.format(x.action, x.score) for x in scored_acts]), file=sys.stderr)
+                #print('\n'.join(['{} {:.4g}'.format(x.action, x.score) for x in scored_acts]), file=sys.stderr)
                 #print('\n', file=sys.stderr)
 
             num_acts = 0
@@ -489,7 +480,7 @@ class Parser(object):
                 action = scored_action.action
                 score = scored_action.score
 
-                if not train_mode:
+                if train_file is None:
                     # If parsing, verify the validity of the action.
                     if not self.is_valid_action(action, ucnt, sent, stack):
                         continue
@@ -517,7 +508,7 @@ class Parser(object):
 
         # Done parsing.  Print the result(s).
         # TODO have this return nltk.tree objects?
-        if not train_mode:
+        if train_file is None:
             if self.n_best > 1:
                 for tree in completetrees:
                     print(tree["score"])
--- discourseparsing/rst_parse.py
+++ discourseparsing/rst_parse.py
@@ -1,55 +1,68 @@
 #!/usr/bin/env python3
 
-from discourseparsing.discourse_parsing import Parser
-from collections import defaultdict
 import re
+from collections import defaultdict
+
+from discourseparsing.discourse_parsing import Parser
 
 
 def main():
     import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--train', action='store_true')
-    parser.add_argument('-m', '--modelpath')
-    parser.add_argument('input_path', help='file to parse, with one EDU \
-                         per line, with POS tags \
-                         (e.g., "This/DT is/VBZ a/DT test/NN ./.").')
-    parser.add_argument('--max_states', type=int, default=1)
-    parser.add_argument('--max_acts', type=int, default=1)
-    parser.add_argument('--n_best', type=int, default=1)
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('input_file',
+                        help='file to parse, with one EDU per line, with POS \
+                              tags (e.g., "This/DT is/VBZ a/DT test/NN ./.").',
+                        type=argparse.FileType('r'))
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-m', '--model_file',
+                       help='Path to model file.',
+                       type=argparse.FileType('r'))
+    group.add_argument('-t', '--train_file',
+                       help='Path to training file.',
+                       type=argparse.FileType('r'))
+    parser.add_argument('-a', '--max_acts',
+                        help='Maximum number of actions for...?',
+                        type=int, default=1)
+    parser.add_argument('-n', '--n_best',
+                        help='Number of parses to return', type=int, default=1)
+    parser.add_argument('-s', '--max_states',
+                        help='Maximum number of states to retain for \
+                              best-first search',
+                        type=int, default=1)
     args = parser.parse_args()
 
     parser = Parser(max_acts=args.max_acts,
                     max_states=args.max_states,
                     n_best=args.n_best)
 
-    if not args.train:
+    if args.train_file is None:
         # read the model
         weights = defaultdict(dict)
-        with open(args.modelpath) as model_file:
-            for line in model_file:
-                parts = line.strip().split()
-                weights[parts[0]][parts[1]] = float(parts[2])
+        for line in args.model_file:
+            parts = line.strip().split()
+            weights[parts[0]][parts[1]] = float(parts[2])
         parser.set_weights(weights)
 
-    with open(args.input_path) as f:
-        data = f.read().strip()
-        docs = re.split(r'\n\n+', data)
+    data = args.input_file.read().strip()
+    docs = re.split(r'\n\n+', data)
 
-        for doc in docs:
-            # Split the document into edus, one edu per line (with POS tags)
-            # e.g., This/DT is/VBZ a/DT test/NN ./."
-            # TODO change this to read in the JSON format that also includes
-            # PTB trees.
-            edus = []
-            for edu_str in doc.split("\n"):
-                edu = []
-                for tagged_token in edu_str.strip().split():
-                    slash_idx = tagged_token.rindex('/')
-                    edu.append((tagged_token[:slash_idx],
-                                tagged_token[slash_idx + 1:]))
-                edus.append(edu)
+    for doc in docs:
+        # Split the document into edus, one edu per line (with POS tags)
+        # e.g., This/DT is/VBZ a/DT test/NN ./."
+        # TODO change this to read in the JSON format that also includes
+        # PTB trees.
+        edus = []
+        for edu_str in doc.split("\n"):
+            edu = []
+            # Don't split on all whitespace because of crazy characters
+            for tagged_token in edu_str.strip().split(' '):
+                slash_idx = tagged_token.rindex('/')
+                edu.append((tagged_token[:slash_idx],
+                            tagged_token[slash_idx + 1:]))
+            edus.append(edu)
 
-            tree = parser.parse(edus)
+        parser.parse(edus, train_file=args.train_file)
 
 
 if __name__ == '__main__':
