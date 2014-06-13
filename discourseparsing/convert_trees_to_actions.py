6c43c86c3e3e0575cd1005969d99196b7af45f32
Michael Heilman <mheilman@ets.org>
2014-06-13 16:39:42 -0400

    integrated rst dependency computation into convert_trees_to_actions.py.  no need for a separate dependency file now

--- discourseparsing/convert_trees_to_actions.py
+++ discourseparsing/convert_trees_to_actions.py
@@ -12,7 +12,7 @@ be space-separated.
 '''
 
 import argparse
-from nltk.tree import Tree
+from nltk.tree import ParentedTree
 from collections import namedtuple
 import re
 
@@ -20,40 +20,61 @@ import re
 ShiftReduceAction = namedtuple('ShiftReduceAction', ['type', 'label'])
 
 
-def dependencies_generator(dep_file):
-    dtree_strings = re.split(r'\n\n+', dep_file.read().strip())
-    for dtree_string in dtree_strings:
-        dtree = []
-        dtree.append({'idx': 0, 'word': "leftwall", 'pos': "LW", 'link': -1})
-
-        parts = dtree_string.split('\n')
-        for part in parts:
-            a = re.split(r'[ \t]', part)
-            dtree.append({'idx': a[0],
-                          'word': a[1],
-                          'lemma': a[2],
-                          'cpos': a[3],
-                          'pos': a[4],
-                          'morph': a[5],
-                          'link': a[6]})
-        yield dtree
+# def _find_rst_heads(tree, index_list, heads):
 
 
-def convert_trees_to_actions(constituent_file, dep_file):
+#     # preterminal (stopping condition)
+#     if isinstance(tree[0], str):
+#         heads[index] = index
+#         return
+
+#     for i, child in enumerate(tree):
+#         _find_rst_heads(tree, index, heads)
+
+
+
+# def compute_rst_dependencies(tree):
+#     dtree = []
+#     dtree.append({'idx': 0, 'word': "leftwall", 'pos': "LW", 'link': -1})
+
+#     # Do a post-order traversal of the tree and
+#     # set the head of each node to be the head of the leftmost of its children
+#     # (or itself if it is a preterminal).
+#     _find_rst_heads(tree, [0], {})
+
+
+#     # For each preterminal, walk up the tree until the current node's head is 
+#     # not the preterminal.  Then, set the preterminal's head to be that node's
+#     # head.  Add the resulting dependency.
+
+#     for part in parts:
+#         a = re.split(r'[ \t]', part)
+#         dtree.append({'idx': a[0],
+#                       'word': a[1],
+#                       'lemma': a[2],
+#                       'cpos': a[3],
+#                       'pos': a[4],
+#                       'morph': a[5],
+#                       'link': a[6]})
+#     return dtree
+
+
+def convert_trees_to_actions(constituent_file):
     tree = ""
 
-    for line, dtree in zip(constituent_file, dependencies_generator(dep_file)):
-        tree = Tree(line.strip())
+    for line in constituent_file:
+        tree = ParentedTree(line.strip())
         tree.set_label('ROOT')
+        #dtree = compute_rst_dependencies(tree)
 
         stack = []
-        cstack = [{'nt': 'LEFTWALL', 'parent': 'LEFTWALL', 'tree': ''}]
-        dstack = [0]
-        dptr = [0]  # this is a list so it's mutable and passed by reference
+        cstack = [ParentedTree('(DUMMY0 (DUMMY1 DUMMY3))')]
+        #dstack = [0]
+        #dptr = [0]  # this is a list so it's mutable and passed by reference
 
         actseq = []
 
-        _convert_tree_to_actions_helper(tree, stack, cstack, dstack, dptr, dtree, actseq)
+        _convert_tree_to_actions_helper(tree, stack, cstack, actseq)
 
         actseq = merge_constituent_end_shifts(actseq)
 
@@ -80,27 +101,34 @@ def merge_constituent_end_shifts(actseq):
             res.append(act)
     return res
 
+def _is_head_of(n1, n2):
+    n1parent = n1.parent()
+    if n2.parent() != n1parent:
+        return False
 
-def _convert_tree_to_actions_helper(node, stack, cstack, dstack, dptr_list, dtree, actseq):
+    if n1.label().startswith('nucleus:'):
+        if n2.label().startswith('satellite:'):
+            return True
+        elif n1parent.index(n1) < n1parent.index(n2):
+            return True
+    return False
+
+
+def _convert_tree_to_actions_helper(node, stack, cstack, actseq):
     stack.append(node)
 
     for child in node:
-        # TODO need this???
         if isinstance(child, str):
             continue
-        _convert_tree_to_actions_helper(child, stack, cstack, dstack, dptr_list, dtree, actseq)
+        _convert_tree_to_actions_helper(child, stack, cstack, actseq)
 
     nt = stack.pop()
 
     # If the current node is a preterminal, add a shift action.
-    tmp_parent = cstack[-1]['parent']
+    tmp_parent = cstack[-1].parent()
     if isinstance(nt[0], str):
         actseq.append(ShiftReduceAction(type='S', label='POS'))
-        cstack.append({'nt': '{}(from shift)'.format(nt),
-                       'parent': stack[-1],
-                       'tree': '({})'.format(nt)})
-        dptr_list[0] += 1
-        dstack.append(dptr_list[0])
+        cstack.append(nt)
     # Or if we are at the root of the tree, then add reduce_right:ROOT.
     elif tmp_parent.label() == "ROOT":
         actseq.append(ShiftReduceAction(type='R', label='ROOT'))
@@ -109,11 +137,8 @@ def _convert_tree_to_actions_helper(node, stack, cstack, dstack, dptr_list, dtre
     # and we should add a unary reduce
     else:
         actseq.append(ShiftReduceAction(type='U', label=tmp_parent.label()))
-        tmpc = cstack.pop()
-        cstack.append({'nt': '{}(from reduce = {})'.format(tmpc['nt'],
-                                                           tmpc['parent']),
-                       'parent': stack[-1],
-                       'tree': '({} {})'.format(tmpc['parent'], tmpc['tree'])})
+        cstack.pop()
+        cstack.append(nt)
 
     # Check to see if there should be any reduce right or reduce left actions.
     chflg = True
@@ -123,56 +148,38 @@ def _convert_tree_to_actions_helper(node, stack, cstack, dstack, dptr_list, dtre
         # is the 2nd most recently visited node,
         # and they have the same parent,
         # then add a reduce_right.
-        if dtree[dstack[-1]]['link'] == dtree[dstack[-2]]['idx'] \
-                and cstack[-1]['parent'] == cstack[-2]['parent']:
-            tmpR = dstack.pop()
-            tmpL = dstack.pop()
-            dstack.append(tmpL)
+        if _is_head_of(cstack[-2], cstack[-1]):
 
             tmpRc = cstack.pop()
             tmpLc = cstack.pop()
-            cstack.append({'parent': stack[-1],
-                           'nt': tmpLc['nt'],
-                           'tree': '{} {}'.format(tmpLc['tree'],
-                                                  tmpRc['tree'])})
-            tmp_parent = cstack[-1]['parent']
+            cstack.append(tmpLc)
 
             actseq.append(ShiftReduceAction(type='R',
-                                            label='{}*'.format(tmp_parent.label())))
-
+                                            label='{}*'.format(tmpLc.parent().label())))
             chflg = True
 
         # If the most recently visited node
         # is the head of the 2nd most recently visited node
         # and they both have the same parent,
         # then add a reduce_left.
-        if dtree[dstack[-2]]['link'] == dtree[dstack[-1]]['idx'] \
-                and cstack[-1]['parent'] == cstack[-2]['parent']:
-            tmpR = dstack.pop()
-            tmpL = dstack.pop()
-            dstack.append(tmpR)
+        if _is_head_of(cstack[-1], cstack[-2]):
 
             tmpRc = cstack.pop()
             tmpLc = cstack.pop()
-            cstack.append({'parent': stack[-1],
-                           'nt': tmpRc['nt'],
-                           'tree': '{} {}'.format(tmpLc['tree'], tmpRc['tree'])})
-            tmp_parent = cstack[-1]['parent']
+            cstack.append(tmpRc)
 
             actseq.append(ShiftReduceAction(type='L',
-                                            label='{}*'.format(tmp_parent.label())))
-
+                                            label='{}*'.format(tmpRc.parent().label())))
             chflg = True
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('mrg_path', help='a file with constituent trees in mrg format.')
-    parser.add_argument('dep_path', help='the corresponding dependency file in CoNLL-X format.')
     args = parser.parse_args()
 
-    with open(args.mrg_path) as constituent_file, open(args.dep_path) as dep_file:
-        for actseq in convert_trees_to_actions(constituent_file, dep_file):
+    with open(args.mrg_path) as constituent_file:
+        for actseq in convert_trees_to_actions(constituent_file):
             print(' '.join(['{}:{}'.format(x.type, x.label) for x in actseq]))
 
 
