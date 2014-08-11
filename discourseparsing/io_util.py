e18ad4e202690e57913a730e212e770011a52c90
Michael Heilman <mheilman@ets.org>
2014-08-11 09:49:33 -0400

    modified io_util.py to try decoding as utf-8 first (since cchardet is wrong sometimes)

--- discourseparsing/io_util.py
+++ discourseparsing/io_util.py
@@ -9,10 +9,16 @@ def read_text_file(input_path):
     '''
     with open(input_path, 'rb') as input_file:
         doc = input_file.read()
-        chardet_output = cchardet.detect(doc)
-        encoding = chardet_output['encoding']
-        encoding_confidence = chardet_output['confidence']
-        logging.debug('decoding {} as {} with {} confidence'
-                      .format(input_path, encoding, encoding_confidence))
-        doc = doc.decode(encoding)
+        # Try decoding as utf-8 first.  Then, if that doesn't work, try using
+        # the cchardet module to auto-detect the encoding.
+        try:
+            doc = doc.decode('utf-8')
+        except UnicodeDecodeError:
+            chardet_output = cchardet.detect(doc)
+            encoding = chardet_output['encoding']
+            encoding_confidence = chardet_output['confidence']
+            logging.debug('decoding {} as {} with {} confidence'
+                          .format(input_path, encoding, encoding_confidence))
+            doc = doc.decode(encoding)
+
     return doc
