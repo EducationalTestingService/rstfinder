815c30c838b8ccaa43e4a5bf61b724475a823293
Aoife Cahill <acahill@ets.org>
2015-04-24 11:34:54 -0400

    change python header to be python instead of python3

--- discourseparsing/rst_eval.py
+++ discourseparsing/rst_eval.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # License: MIT
 
 from collections import Counter
--- discourseparsing/rst_parse_batch.py
+++ discourseparsing/rst_parse_batch.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # License: MIT
 
 import json
