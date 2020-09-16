39f4485087700292bf7714e4c63a04f946a54d43
Nitin Madnani <nmadnani@ets.org>
2020-09-16 10:23:40 -0400

    Fix some typos.

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,17 +1,17 @@
 #!/bin/zsh
 
-# set up some paths
+# set up the conda paths
 CURRDIR=$(pwd)
 CONDA_ENV_PATH="${CURRDIR}"/../parserdev
 
 # remove the conda environment if it already exists
 [[ -d "${CONDA_ENV_PATH}" ]] && /opt/python/conda_default/bin/conda env remove -p "${CONDA_ENV_PATH}"
 
-# create conda environment
+# create the conda environment
 CONDA_ENV_PATH="${CURRDIR}"/../parserdev
 /opt/python/conda_default/bin/conda create --override-channels -c conda-forge -c ets -c https://nlp.research.ets.org/conda -p "${CONDA_ENV_PATH}" python=3.6 nose crf++ --file conda_requirements.txt
 
-# install the bookadmin package in development mode
+# install the rstfinder package in development mode
 "${CONDA_ENV_PATH}"/bin/pip install -e .
 
 # run the tests and generate the coverage report
