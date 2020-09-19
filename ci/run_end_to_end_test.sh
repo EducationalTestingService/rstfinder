2215fc4825021dec44cd2c5744975ed83db1087a
Nitin Madnani <nmadnani@ets.org>
2020-09-18 21:11:44 -0400

    Fix bug in test script.

--- ci/run_end_to_end_test.sh
+++ ci/run_end_to_end_test.sh
@@ -26,20 +26,20 @@ export CORPORA="/home/nlp-text/static/corpora"
 "${CONDA_ENV_PATH}"/bin/tune_segmentation_model rst_discourse_tb_edus_features_TRAINING_TRAIN.tsv rst_discourse_tb_edus_features_TRAINING_DEV.tsv segmentation_model >& tune_segmenter.log
 
 # save the best segmenter output
-tail -6 tune_segmenter.log > best_segmenter_f1
+tail -6 tune_segmenter.log > best_segmenter_f1.txt
 
 # train the RST parser
 "${CONDA_ENV_PATH}"/bin/tune_rst_parser rst_discourse_tb_edus_TRAINING_TRAIN.json rst_discourse_tb_edus_TRAINING_DEV.json rst_parsing_model >& tune_rst_parser.log
 
 # get best F1 value and check that it is within expected limits
-F1VALUE=$(tail -1 nohup.out | grep -o '0\.[0-9]\+[^$]' | sed 's/,//')    
-echo "$F1VALUE" > best_rst_parser_f1
+F1VALUE=$(tail -1 tune_rst_parser.log | grep -o '0\.[0-9]\+[^$]' | sed 's/,//')
+echo "$F1VALUE" > best_rst_parser_f1.txt
 echo "${F1VALUE} > 0.58 && ${F1VALUE} < 0.60" | bc -l
 
 # run any of the trained models on a test document
 # NOTE: it doesn't matter which ones we run since
 # we just want to make sure that the `rst_parse` command runs
-"${CONDA_ENV_PATH}"/bin/rst_parse -g segmentation_model.C8.0 -p rst_parsing_model.C0.5 tests/data/rst_document.txt > output.json
+"${CONDA_ENV_PATH}"/bin/rst_parse -g segmentation_model.C8.0 -p rst_parsing_model.C0.5 tests/data/rst_document.txt > output.json.txt
 
 # run the visualizer next
 visualize_rst_tree output.json tree.html --embed_d3js
