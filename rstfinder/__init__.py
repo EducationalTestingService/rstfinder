1ba617690bbf50648a096875f419774064d284a6
Nitin Madnani <nmadnani@ets.org>
2020-10-01 12:40:25 -0400

    Add check for python zpar when importing rstfinder.

--- rstfinder/__init__.py
+++ rstfinder/__init__.py
@@ -1,3 +1,9 @@
 # Ensure there won't be logging complaints about no handlers being set
 import logging
+
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+try:
+    import zpar
+except ImportError:
+    raise ImportError("The 'python-zpar' package is missing. Run 'pip install python-zpar' to install it.") from None
